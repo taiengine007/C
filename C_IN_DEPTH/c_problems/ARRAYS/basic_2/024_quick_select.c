782f965c11e8fe1642bb63e94b228c7a9dddf9f5
tailung <tailungsrage@gmail.com>
2024-10-18 14:35:38 +0530

    .

---
+++ C_IN_DEPTH/c_problems/ARRAYS/basic_2/024_quick_select.c
@@ -0,0 +1,23 @@
+
+/*
+https://youtu.be/BP7GCALO2v8?si=RligMgrxJhUN-a2P
+      Find the Kth Smallest/Largest Element
+
+Problem: Find the Kth smallest or largest element in an unsorted array.
+Example: The 2nd largest element in [5, 3, 1, 8, 9] is 8.
+Hints: Use sorting or a selection algorithm like QuickSelect.
+*/
+////////////////////////////////////////////////////
+#include<stdio.h>
+
+int main()
+{
+
+  int arr[]={5,3,1,8,9};
+  int n=sizeof(arr)/sizeof(arr[0]);
+
+  //quick select
+
+
+return 0;
+}
